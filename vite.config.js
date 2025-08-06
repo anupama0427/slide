ee592a38f43630786e78b1957a82a22e3dfcbe3f
tavvagnanaanupama@gmail.com <tavvagnanaanupama@gmail.com>
2025-08-06 07:43:16 +0530

    Success Message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   base: '/fed/',
+=======
+>>>>>>> c8f41d2 (Success Message)
   plugins: [react()],
 })
